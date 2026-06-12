26877964cb72ef9a39703eff707bfaada72efff0
devharrish <developerharrish@gmail.com>
2026-06-12 14:25:29 +0530

    commit comparision operator

---
+++ Basics/04_comparitiveOperators.py
@@ -0,0 +1,37 @@
+'''
+
+Comparison operators are used to compare two values. They return either True or False depending on whether the comparison is correct. It is also called Relational Operators.
+
+== : Equal to
+!= : Not equal to
+> : Greater than
+< : Less than
+>= : Greater than or equal to
+<= : Less than or equal to
+
+'''
+
+#Equal to
+
+print(2 == 3)  # Output: False
+
+#Not equal to
+
+print(2 != 3)  # Output: True       
+
+#Greater than
+
+print(2 > 3)  # Output: False
+
+#Less than
+
+print(2 < 3)  # Output: True
+
+#Greater than or equal to
+
+print(2 >= 3)  # Output: False
+
+#Less than or equal to
+
+print(2 <= 3)  # Output: True
+
