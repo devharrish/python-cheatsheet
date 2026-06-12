b00284e766cdc8aaff69e5e32e924ea60381ea55
devharrish <developerharrish@gmail.com>
2026-06-12 13:31:56 +0530

    Arithmetic Operators

---
+++ Functions_and_Variables/arithmeticOperator.py
@@ -0,0 +1,107 @@
+'''
+
+Arithmetic Operators are used to perform mathematical operations like addition, subtraction, multiplication, division etc.
+
+'''
+
+#Addition
+
+print(2 + 3)  # Output: 5
+
+#By using variables
+
+num1 = 2
+num2 = 3
+
+print(num1 + num2)  # Output: 5
+
+#Subtraction
+
+print(5 - 3)  # Output: 2
+
+#By using variables
+
+num1 = 5
+num2 = 3
+
+print(num1 - num2)  # Output: 2
+
+#Multiplication
+
+print(2 * 3)  # Output: 6
+
+#By using variables
+
+num1 = 2
+num2 = 3
+
+print(num1 * num2)  # Output: 6
+
+'''
+
+In division, it returns the quotient of two numbers.
+
+'''
+
+#Division
+
+print(10 / 2)  # Output: 5.0
+
+#By using variables
+
+num1 = 10
+num2 = 2
+
+print(num1 / num2)  # Output: 5.0
+
+'''
+
+In modulus, it returns the remainder of two numbers.
+
+'''
+
+#Modulus
+
+print(10 % 3)  # Output: 1
+
+#By using variables
+
+num1 = 10
+num2 = 3
+
+print(num1 % num2)  # Output: 1
+
+'''
+
+In floor division, it returns the quotient of two numbers rounded down to the nearest integer.
+
+'''
+
+#Floor Division
+
+print(10 // 3)  # Output: 3
+
+#By using variables
+
+num1 = 10
+num2 = 3
+
+print(num1 // num2)  # Output: 3
+
+'''
+
+In exponent, it returns the power of base raised to the exponent eg:2^3.
+
+'''
+
+#Exponent
+
+print(2 ** 3)  # Output: 8
+
+#By using variables
+
+num1 = 2
+num2 = 3
+
+print(num1 ** num2)  # Output: 8
+
