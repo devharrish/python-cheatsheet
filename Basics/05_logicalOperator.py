c0d10ef0a3d4138487bf66952b50ebd122beb193
devharrish <developerharrish@gmail.com>
2026-06-12 15:46:59 +0530

    commit logical operator

---
+++ Basics/05_logicalOperator.py
@@ -0,0 +1,10 @@
+'''
+
+Logical operators are used to combine two or more conditions. They return either True or False based on the evaluation of the conditions. It is also called Boolean Operators.
+
+and : Logical AND
+or : Logical OR
+not : Logical NOT
+
+'''
+
