15ec10df340548775f03439a9a585983de0ec340
devharrish <developerharrish@gmail.com>
2026-06-12 13:20:42 +0530

    Variables

---
+++ Functions_and_Variables/variables.py
@@ -0,0 +1,25 @@
+'''
+
+Variables are containers for storing data values. It is used to store data values that can be used to perform operations on them.
+
+'''
+
+#Declaring a variable (name) and store value (Harrish)
+
+name = "Harrish"
+
+#print the name
+
+print(name)
+
+#Accessing the variable name
+
+print("My name is", name) 
+
+'''
+Output:
+
+Harrish
+My name is Harrish
+
+'''
