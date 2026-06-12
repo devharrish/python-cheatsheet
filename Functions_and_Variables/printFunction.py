8d62c22db2283873c8cd01805fe7f316d11e4bfa
devharrish <developerharrish@gmail.com>
2026-06-12 13:05:18 +0530

    print function

---
+++ Functions_and_Variables/printFunction.py
@@ -0,0 +1,12 @@
+'''
+The print() function is a built-in Python function used to display output on the screen (console).
+
+'''
+
+#Print a string
+
+print("Hello World")
+
+#Using separate arguments
+
+print("Hello", "World", "!")
