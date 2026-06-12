72914048d52a1041e7084b8604257afe70367598
devharrish <developerharrish@gmail.com>
2026-06-12 13:55:45 +0530

    commit Comment file

---
+++ Basics/00_Comments.py
@@ -0,0 +1,25 @@
+'''
+
+Comments are lines of text in a Python program that are used to explain the code. Python ignores comments when running the program, so they do not affect the output.
+
+There are two types of comments in Python:
+
+Single-line comments: These are comments that start with a hash symbol (#).
+Multi-line comments: These are comments that start with a quotation symbol (""") and end with a quotation symbol (""").
+
+
+'''
+
+#Single-line comment
+
+"""
+
+Multi-line comment
+
+"""
+
+'''
+
+This is also a multi-line comment
+
+'''
