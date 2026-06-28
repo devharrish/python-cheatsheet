9cbd9490d5060a1a10285399729fe6e45b1fd61d
devharrish <developerharrish@gmail.com>
2026-06-28 15:22:35 +0530

    Add Syntax.py file

---
+++ Collections/List/syntax.py
@@ -0,0 +1,24 @@
+'''
+
+A Python list is a built-in, mutable, and ordered collection data type designed to store multiple items in a single variable.
+
+Lists are defined by placing comma-separated values inside square brackets [].
+
+'''
+
+'''
+
+Syntax
+
+'''
+
+list = [] # Empty list
+
+list = [1, 2, 3, 4, 5]
+
+print(list)
+
+
+#Output :
+
+#[1, 2, 3, 4, 5]
